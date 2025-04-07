f947454aca8df05ba9ec82321b595e055a3d6d82
anuj darji <anujdarji@anujs-MacBook-Air.local>
2025-04-07 09:44:32 +0530

    usercontroller changes

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -70,10 +70,11 @@ export const login = async (req, res) => {
 
         res.cookie("token", token, {
             httpOnly: true,
-            secure: process.env.NODE_ENV === "production",
-            sameSite: "strict",
-            maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
+            secure: true,        // Required for HTTPS (Vercel is HTTPS)
+            sameSite: "None",    // Required for cross-origin cookies
+            maxAge: 7 * 24 * 60 * 60 * 1000,
         });
+        
 
         return res.status(200).json({
             success: true,
